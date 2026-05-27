2ee164e3e645ffaaf4eba7d3175badbc0d2ef020
Pau Meló Climent <pau@debian.RyperBlack>
2026-05-27 22:54:43 +0200

    3121-n.special characters 2

--- 3121-Number_of_special_char_2.c
+++ 3121-Number_of_special_char_2.c
@@ -1,14 +1,16 @@
 /*
-3120. Count the Number of Special Characters I
+3121. Count the Number of Special Characters II
 
-You are given a string word. A letter is called special if it appears both in lowercase and uppercase in word.
+You are given a string word. A letter c is called special if it appears both in lowercase and uppercase in word,
+and every lowercase occurrence of c appears before the first uppercase occurrence of c.
 Return the number of special letters in word.
 
 Input: word = "aaAbcBC"
 Output: 3
 
 Explanation:
-The special characters in word are 'a', 'b', and 'c'.
+The special characters are 'a', 'b', and 'c'.
+
 */
 
 #include <stdbool.h>
@@ -21,6 +23,12 @@ int numberOfSpecialChars(char* word) {
     for (int i = 0; word[i] != '\0'; i++) {
         char c = word[i];
         if (c >= 'a' && c <= 'z') {
+            if(upper[c- 'a'])
+            {
+                if(lower[c- 'a'])
+                    lower[c- 'a'] = false;
+                continue;
+            }
             lower[c - 'a'] = true;
         } else if (c >= 'A' && c <= 'Z') {
             upper[c - 'A'] = true;
